d502e221f93b2643a82eefecc27db97cd4c6d631
Anmol Gupta <anmol.gupta2180@gmail.com>
2023-02-08 19:36:52 -0500

    I Have added the file structure

---
+++ client/src/components/CommentForm/index.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+
+import { ADD_COMMENT } from '../../utils/mutations';
+
+const CommentForm = ({ thoughtId }) => {
+  const [commentText, setCommentText] = useState('');
+  const [characterCount, setCharacterCount] = useState(0);
+
+  const [addComment, { error }] = useMutation(ADD_COMMENT);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addComment({
+        variables: { thoughtId, commentText },
+      });
+
+      setCommentText('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'commentText' && value.length <= 280) {
+      setCommentText(value);
+      setCharacterCount(value.length);
+    }
+  };
+
+  return (
+    <div>
+      <h4>What are your thoughts on this thought?</h4>
+      <p
+        className={`m-0 ${
+          characterCount === 280 || error ? 'text-danger' : ''
+        }`}
+      >
+        Character Count: {characterCount}/280
+        {error && <span className="ml-2">Something went wrong...</span>}
+      </p>
+      <form
+        className="flex-row justify-center justify-space-between-md align-center"
+        onSubmit={handleFormSubmit}
+      >
+        <div className="col-12 col-lg-9">
+          <textarea
+            name="commentText"
+            placeholder="Add your comment..."
+            value={commentText}
+            className="form-input w-100"
+            style={{ lineHeight: '1.5' }}
+            onChange={handleChange}
+          ></textarea>
+        </div>
+
+        <div className="col-12 col-lg-3">
+          <button className="btn btn-primary btn-block py-3" type="submit">
+            Add Comment
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CommentForm;
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,32 @@
+import { gql } from '@apollo/client';
+
+export const ADD_THOUGHT = gql`
+  mutation addThought($thoughtText: String!, $thoughtAuthor: String!) {
+    addThought(thoughtText: $thoughtText, thoughtAuthor: $thoughtAuthor) {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+      }
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation addComment($thoughtId: ID!, $commentText: String!) {
+    addComment(thoughtId: $thoughtId, commentText: $commentText) {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
+      }
+    }
+  }
+`;
