ed1baabd78a5967dca364dbb00463e8b965130a7
Kush Patel <kushrpatel06@yahoo.com>
2023-02-15 19:17:53 -0500

    pushing boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,67 @@
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
+import {ApolloProvider,ApolloClient,InMemoryCache,createHttpLink} from "@apollo/client";
+import {setContext} from '@apollo/client/link/context'
 
-import Home from './pages/Home';
-import SingleThought from './pages/SingleThought';
-import Header from './components/Header';
-import Footer from './components/Footer';
+
+// const client = new ApolloClient({
+//   request: operation => {
+//     const token = localStorage.getItem("id_token");
+
+//     operation.setContext({
+//       headers: {
+//         authorization: token ? `Bearer ${token}` : ``
+//       }
+//     });
+//   },
+//   uri: "/graphql"
+// });
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: '/graphql',
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
+    // <div>
+    //   TEST
+    // </div>
     <ApolloProvider client={client}>
-      <Router>
-        <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
-          <div className="container">
-            <Routes>
-              <Route 
-                path="/" 
-                element={<Home />} 
-              />
-              {/* Create a route to display a single thought's comments based on its `thoughtId` provided in the URL */}
-              <Route 
-                path="/thoughts/:thoughtId" 
-                element={<SingleThought />} 
-              />
-            </Routes>
-          </div>
-          <Footer />
-        </div>
-      </Router>
+ <Router>
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={SearchBooks} />
+          <Route exact path='/saved' component={SavedBooks} />
+          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+        </Switch>
+      </>
+    </Router>
+    {/* <div>TEST</div> */}
     </ApolloProvider>
+   
   );
 }
 
