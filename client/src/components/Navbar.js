0f964e26243462287c760080b40b73e60f72e1d6
Kush Patel <kushrpatel06@yahoo.com>
2023-02-16 19:04:05 -0500

    Search Anime Function Works homepage/search anime page can now search for top 10 search results from the user input fixed add to anime list function saved anime now loads in saved anime page, remove anime works renamed biolerplate hearders and nav removed some console.logs so that devs dont run out of calls (anmol)

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -15,19 +15,19 @@ const AppNavbar = () => {
       <Navbar bg='dark' variant='dark' expand='lg'>
         <Container fluid>
           <Navbar.Brand as={Link} to='/'>
-            Google Books Search
+            Animanga Search
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='navbar' />
           <Navbar.Collapse id='navbar'>
             <Nav className='ml-auto'>
               <Nav.Link as={Link} to='/'>
-                Search For Books
+                Search For Anime
               </Nav.Link>
               {/* if user is logged in show saved books and logout */}
               {Auth.loggedIn() ? (
                 <>
                   <Nav.Link as={Link} to='/saved'>
-                    See Your Books
+                    See Your Anime
                   </Nav.Link>
                   <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
                 </>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -57,15 +57,27 @@ export const searchGoogleBooks = (query) => {
 };
 
 
-export const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'b9076a1794msh0313b8f4ae9404fp1b23a5jsnfb26084b05be',
-		'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
-	}
-};
-
-fetch('https://anime-db.p.rapidapi.com/anime?page=1&size=50&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+export const anime= (query)=>{
+    const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'f00967f704mshcdc1a904b456533p1d9212jsn0bb094eab6a5',
+      'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
+    }
+  };
+  
+  return fetch(`https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=${query}`, options)
+    // .then(response => response.json())
+    // .then(response => {console.log(response)
+    // const bookData = response.data.map(book => (
+    //     {
+    //     bookId: book._id,
+    //     authors: book.status || ["No author to display"],
+    //     title: book.title,
+    //     // description: book.volumeInfo.description,
+    //     image: book.image || "",
+    //   }
+    //   ))
+    //   console.log(bookData) })
+    // .catch(err => console.error(err));
+}
