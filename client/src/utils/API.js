8fa8619d1e8d4318163bbd1a78683927b2d85282
Anmol Gupta <anmol.gupta2180@gmail.com>
2023-02-20 18:42:39 -0500

    We have worked on the top anime page and was able to call the api and fetch it on loading page

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -52,9 +52,9 @@ export const deleteBook = (bookId, token) => {
 
 // make a search to google books api
 // https://www.googleapis.com/books/v1/volumes?q=harry+potter
-export const searchGoogleBooks = (query) => {
-  return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
-};
+// export const searchGoogleBooks = (query) => {
+//   return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+// };
 
 export const anime = (query) => {
   const options = {
